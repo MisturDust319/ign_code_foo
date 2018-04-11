363f4aa1f4bfe285b3901c66dd507eb5af841570
Stan Slupecki <jarlofearls@gmail.com>
2018-04-11 15:28:56 -0400

    Adds Road and Tile components

---
+++ src/components/Road.js
@@ -0,0 +1,38 @@
+﻿import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table } from 'reactstrap';
+
+import { Tile } from './tile.js';
+
+function Road(props) {
+    return (
+        <Table>
+            <tr>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+            </tr>
+            <tr>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+            </tr>
+            <tr>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+            </tr>
+            <tr>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+                <Tile value="O"></Tile>
+            </tr>
+        </Table>
+    )
+}
+
+export { Road };
---
+++ src/components/tile.js
@@ -0,0 +1,10 @@
+﻿import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Tile(props) {
+    return (
+        <td>{props.value}</td>
+    )
+}
+
+export { Tile };
