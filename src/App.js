98f43cd1d1442506583062427b73152c34987b8e
Stan Slupecki <jarlofearls@gmail.com>
2018-04-11 14:16:24 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+          <Button color="danger">Danger!</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
