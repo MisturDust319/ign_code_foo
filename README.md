babdae4270797338fee44f9272a849a2ed02d900
Stan Slupecki <jarlofearls@gmail.com>
2018-04-11 19:10:39 -0400

    Some minor upkeep and formatting

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Setup guide
 1. Install node.js and npm for your respective system
 2. from a terminal/command prompt, while in the project directory, type npm install to get all dependancies
 3. after installing dependancies, type npm start and the react app should run in browser
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import logo from './logo.svg';
+import './App.css';
+
 import { Button } from 'reactstrap';
+import { Road } from './components/Road.js';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            tiles: Array(16).fill("O")
+        }
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-          <Button color="danger">Danger!</Button>
-      </div>
+        <Road value={this.state.tiles}></Road>
     );
   }
+
+    toggleTile(i) {
+        this.state.tiles[i] = !this.state.tiles[i];
+
+        this.setState({
+            tiles: this.state.tiles
+        });
+    }
 }
 
 export default App;
