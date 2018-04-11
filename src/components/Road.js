31b96afd670ff753253cd311ea24b3d33d66d37c
Stan Slupecki <jarlofearls@gmail.com>
2018-04-11 19:06:18 -0400

    Simple version of Road working

--- src/components/Road.js
+++ src/components/Road.js
@@ -4,32 +4,39 @@ import { Table } from 'reactstrap';
 
 import { Tile } from './tile.js';
 
+
+
 function Road(props) {
+    let renderTile = (i) => {
+        return (
+            <Tile value={props.value[i]} />
+        )
+    }
     return (
         <Table>
             <tr>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
+                {renderTile(0)}
+                {renderTile(2)}
+                {renderTile(3)}
+                {renderTile(4)}
             </tr>
             <tr>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
+                {renderTile(5)}
+                {renderTile(6)}
+                {renderTile(7)}
+                {renderTile(8)}
             </tr>
             <tr>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
+                {renderTile(9)}
+                {renderTile(10)}
+                {renderTile(11)}
+                {renderTile(12)}
             </tr>
             <tr>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
-                <Tile value="O"></Tile>
+                {renderTile(13)}
+                {renderTile(14)}
+                {renderTile(15)}
+                {renderTile(16)}
             </tr>
         </Table>
     )
