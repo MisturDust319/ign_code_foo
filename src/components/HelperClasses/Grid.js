05429a8471d2f213850c0fb389a28fe8e33ee6cf
Stan Slupecki <jarlofearls@gmail.com>
2018-04-13 19:04:18 -0400

    Changes how grid responds to out of bounds requests

--- src/components/HelperClasses/Grid.js
+++ src/components/HelperClasses/Grid.js
@@ -27,10 +27,10 @@
 
         //check if value is within bounds
         if (x > this.width - 1) {
-            throw "The x value exceeds the width of the grid";
+            return -1;
         }
         if (y > this.height - 1) {
-            throw "This y value exceeds the height of the grid";
+            return -1;
         }
 
         // calculate the position in the 2d array
@@ -42,11 +42,19 @@
     setValue(x, y, value) {
         let pos = this.calculatePosition(x, y);
 
+        if (pos === -1) {
+            throw "Input out of bounds";
+        }
+
         this.arr[pos] = value;
     }
 
     getValue(x, y) {
         let pos = this.calculatePosition(x, y);
+
+        if (pos === -1) {
+            return -1;
+        }
 
         return this.arr[pos];
     }
