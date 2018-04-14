b1c5373c7c46fade543cf53e838c59b29d6b862d
Stan Slupecki <jarlofearls@gmail.com>
2018-04-14 17:40:26 -0400

    Some reason this wasn't added earlier

---
+++ src/components/MoveHistory.js
@@ -0,0 +1,34 @@
+﻿import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { ListGroup } from 'reactstrap';
+import { ListGroupItem } from 'reactstrap';
+
+function MoveHistory(props) {
+
+    function mapMoves() {
+        // an id to be used as the move's key
+        let id = 999;
+        const ret = props.value.map((cur) => {
+            id++; //increment id to keep unique
+
+            var body = cur.map((move) => {
+                return "(" + move.x + ", " + move.y + ") ";
+            });
+
+            return (
+                <ListGroupItem key={id}>{body}</ListGroupItem>
+            )
+        });
+
+        return ret;
+    }
+
+    return (
+        <ListGroup>
+            {mapMoves()}
+        </ListGroup>
+    )
+}
+
+export { MoveHistory };
