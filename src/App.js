3df46514ae6bb9463a14640d3c00b319affbf72e
Stan Slupecki <jarlofearls@gmail.com>
2018-04-16 02:44:27 -0400

    Adds selection header

--- src/App.js
+++ src/App.js
@@ -6,19 +6,39 @@ import './App.css';
 import { Container } from 'reactstrap';
 import { RoadPanel } from './components/RoadPanel.js';
 import { FeedPanel } from './components/FeedPanel.js';
+import { Button, ButtonGroup } from 'reactstrap/dist/reactstrap.es';
 
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            panelSelect : "",
+            view: "road",
         };
+    }
+
+    renderPanel = () => {
+        if(this.state.view == "road") {
+            return ( <RoadPanel />);
+        } else if (this.state.view == "feed") {
+            return ( <FeedPanel />);
+        }
+    }
+
+    goToFeed = () => {
+        this.setState({view: "feed"});
+    }
+    goToRoad = () => {
+        this.setState({view: "road"});
     }
     
     render() {
         return (<Container>
-            <FeedPanel />
+            <ButtonGroup>
+                <Button onClick={this.goToFeed}>Feed</Button>
+                <Button onClick={this.goToRoad}>Road Problem</Button>
+            </ButtonGroup>
+            {this.renderPanel()}
         </Container>);
     }
 }
